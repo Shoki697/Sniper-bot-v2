f44930c1778fab62569e6d442e862adfd02f87a6
Shoki697 <92455767+Shoki697@users.noreply.github.com>
2023-01-19 11:49:49 +0100

    Update main.js

--- main.js
+++ main.js
@@ -19,10 +19,6 @@ const client = new Client({
 
 let connection;
 
-var port = process.env.PORT || 3000;
-app.listen(port, "0.0.0.0", function() {
-console.log("Listening on Port 3000");
-});
 
 client.once('ready', (c) => 
 {
